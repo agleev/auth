2c065ffa5bdf5dcec06496935b58c4bb2c277f2a
Azat Agleev <82861838+agleev@users.noreply.github.com>
2025-10-01 17:35:23 +0300

    add model Email

--- domain.py
+++ domain.py
@@ -1,3 +1,5 @@
+
+import re
 # from pydantic import BaseModel
 
 
@@ -12,3 +14,15 @@ class Token:
     access_token: str
     token_type: str
     expires_in: int
+
+class Email:
+
+    EMAIL_REGEX = re.compile(r"^[A-Za-z0-9]+[A-Za-z0-9._]*[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+$")
+
+    def __init__(self, value):
+        self.value = value
+        if not self._is_valid():
+            raise ValueError
+
+    def _is_valid(self):
+        return self.EMAIL_REGEX.match(self.value) is not None
