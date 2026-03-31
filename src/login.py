ef869860d60f459e96d5a341a1deb07b65141b87
Azat Agleev <82861838+agleev@users.noreply.github.com>
2026-03-31 11:13:25 +0300

    Add login functionality with session checks
    Implement login endpoint with session validation and token generation.

---
+++ src/login.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Response, Request
+import string
+from typing import Optional
+
+app = FastAPI()
+
+
+async def auth_session_validate(session_id: str):
+  pass
+
+async def check_user_app_connection(callback_url: str, auth_session_id: str):
+  pass
+
+@app.get("/login")
+async def check_login_session(
+  request: Request,
+  response: Response,
+  auth_session_id: Optional[str] = None,
+  callback_url: str = None
+):
+  
+  session_valid = await auth_session_validateaurg_session_id()
+  if not session_valid:
+    response.delete_cookie('auth_session_id')
+    return {
+      'status_code': 404, 
+      'message': 'Unauthorized'
+    }
+  
+  conn_valid = await check_user_app_connection(callback_url, auth_session_id)
+  if not conn_valid:
+    response.set_cookie(key='auth_session_id', value=auth_session_id)
+    return {
+      'status_code': 403,
+      'message': 'Forbidden'
+      }
+  session_token = geterate_token()
+
+def geterate_token(length=256):
+  characters = string.ascii_letters + string.digits
+  random_string = ''.join(secrets.choice(characters) for _ in range(length))
+  return random_string
+  
+
+@app.post("/login")
+async def authenticate_user():
+  pass 
