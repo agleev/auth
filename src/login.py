6ad4fd2e723ab51f8271aa5299d11c81e75c2097
Azat Agleev <82861838+agleev@users.noreply.github.com>
2026-04-01 13:04:24 +0300

    Implement user authentication and session management
    Added user authentication and login functionality with session handling.

--- src/login.py
+++ src/login.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, Request
 import string
+import random
 from typing import Optional
 
 app = FastAPI()
@@ -11,6 +12,12 @@ async def auth_session_validate(session_id: str):
 async def check_user_app_connection(callback_url: str, auth_session_id: str):
   pass
 
+async def auth_user_login(
+  login: str,
+  password: str
+):
+  pass
+
 @app.get("/login")
 async def check_login_session(
   request: Request,
@@ -19,7 +26,7 @@ async def check_login_session(
   callback_url: str = None
 ):
   
-  session_valid = await auth_session_validateaurg_session_id()
+  session_valid = await auth_session_validate(auth_session_id)
   if not session_valid:
     response.delete_cookie('auth_session_id')
     return {
@@ -38,10 +45,32 @@ async def check_login_session(
 
 def geterate_token(length=256):
   characters = string.ascii_letters + string.digits
-  random_string = ''.join(secrets.choice(characters) for _ in range(length))
+  random_string = ''.join(random.choice(characters) for _ in range(length))
   return random_string
   
 
 @app.post("/login")
-async def authenticate_user():
-  pass 
+async def authenticate_user(
+  # request: Request,
+  response: Response,
+  login: str,
+  password: str,
+  auth_session_id: str,
+  callback_url: str):
+  
+  auth_valid = auth_user_login(login, password)
+  if not auth_valid:
+    response.delete_cookie("auth_session_id")
+    return {
+      'status_code': 401, 
+      'message': 'Unauthorized'
+    }
+    
+  conn_valid = await check_user_app_connection(callback_url, auth_session_id)
+  if not conn_valid:
+    response.set_cookie(key='auth_session_id', value=auth_session_id)
+    return {
+      'status_code': 403,
+      'message': 'Forbidden'
+      }
+    
